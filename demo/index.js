c5b554b97f217beeafbc3809b6ae03ae8761be9e
Mohammad Umair Khan <mohammad.umair.k@gmail.com>
2015-06-14 23:41:48 +0500

    WIP calendar

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,21 @@
 (function(){
-	
+
 	'use strict';
 
-	angular.module('app', ['ui.bootstrap']);
+	angular.module('app', ['ui.bootstrap'])
+		.controller('appCtrl', function(){
+			var vm = this;
+      var currentYear = new Date().getFullYear();
+      vm.minDate = '1-1-' + currentYear;
+      vm.maxDate = '12-31-' + currentYear;
+
+      vm.inRange = inRange;
+
+      ///////////////
+
+      function inRange(date){
+        //check if incoming date is in date range ?
+      }
+		});
 
 }());
