052469cf5638183ed67450e9eb088078aef96516
Mohammad Umair Khan <umair.khan@tenperals.com>
2015-06-20 06:25:23 +0500

    adding more to build process

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,12 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var ngAnnotate = require('gulp-ng-annotate');
 var uglify = require('gulp-uglify');
+var bump = require('gulp-bump');
+var git = require('gulp-git');
+var tagVersion = require('gulp-tag-version');
+var filter = require('gulp-filter');
 
-var files = [
+var componentSrc = [
   'src/datepicker.js',
   'src/dateparser/dateParser.js',
   'src/position/position.js',
@@ -16,7 +20,12 @@ var files = [
   'src/monthpicker.js',
   'src/yearpicker.js',
   'src/ui.bootstrap.js'
-]
+];
+
+var pkgFiles = [
+  './bower.json',
+  './package.json'
+];
 
 gulp.task('default', function(){
   return gulp.src(files)
@@ -25,3 +34,23 @@ gulp.task('default', function(){
     //.pipe(uglify())
     .pipe(gulp.dest('dist/'));
 });
+
+function inc(importance) {
+  // get all the files to bump version in
+  return gulp.src(pkgFiles)
+    // bump the version number in those files
+    .pipe(bump({type: importance}))
+    // save it back to filesystem
+    .pipe(gulp.dest('./'))
+    // commit the changed version number
+    .pipe(git.commit('bumping ' + importance))
+
+    // read only one file to get the version number
+    .pipe(filter('package.json'))
+    // **tag it in the repository**
+    .pipe(tagVersion());
+}
+
+gulp.task('patch', function() { return inc('patch'); })
+gulp.task('feature', function() { return inc('minor'); })
+gulp.task('release', function() { return inc('major'); })
